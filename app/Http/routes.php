e57b2c27916b03732d6741be75c895d6b4723ebc
GevinHartanto <gevinhartanto@gmail.com>
2017-09-07 20:12:03 +0700

    Many to Many Relationship

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -85,6 +85,15 @@ Route::get('/posts', function(){
 	}
 });
 
+//Many to Many Relationship 
+Route::get('/user/{id}/role', function($id){
+		$user = User::find($id)->roles()->orderBy('id', 'desc')->get();
+		return $user;
+		//foreach($user->roles as $role){
+		//	return $role->name;
+		//}
+});
+
 Route::get('/user/{id}/posts', function($id){
 	$user = User::find($id);
 	
