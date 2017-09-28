f38672111cee886c95beefee7c01c50c79b40889
GevinHartanto <gevinhartanto@gmail.com>
2017-09-28 18:58:10 +0700

    Setting Up Views and Routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -7,221 +7,223 @@ use App\Photo;
 use App\Tag;
 use App\Video;
 
-Route::get('/read', function(){
-	$posts = Post::all();
-	foreach($posts as $post){
-			return $post->title;
-	}
-});
+//Route::get('/read', function(){
+//	$posts = Post::all();
+//	foreach($posts as $post){
+//			return $post->title;
+//	}
+//});
 
-Route::get('/find', function(){
-	$post = Post::find(1);
-	return $post->title;
-});
+//Route::get('/find', function(){
+//	$post = Post::find(1);
+//	return $post->title;
+//});
 
-Route::get('/findwhere', function(){
-	$posts = Post::where('is_admin', 0)->orderBy('id', 'desc')->take(1)->get();
-	return $posts;
-});
+//Route::get('/findwhere', function(){
+//	$posts = Post::where('is_admin', 0)->orderBy('id', 'desc')->take(1)->get();
+//	return $posts;
+//});
 
-Route::get('/basicinsert', function(){
-	$post = new Post;
-	$post->title = 'New Eloquent Title';
-	$post->content = 'Wow Elquent is really cool';
-	$post->save();
-});
+//Route::get('/basicinsert', function(){
+//	$post = new Post;
+//	$post->title = 'New Eloquent Title';
+//	$post->content = 'Wow Elquent is really cool';
+//	$post->save();
+//});
 
-Route::get('/create', function(){
-	Post::create(['title' => 'create method', 'content' => 'saya belajar banyak hari ini']);	
-});
+//Route::get('/create', function(){
+//	Post::create(['title' => 'create method', 'content' => 'saya belajar banyak hari ini']);	
+//});
 
-Route::get('/basicupdate', function(){
-	$post = Post::find(2);
-	$post->title = 'Update Eloquent Title';
-	$post->content = 'Update Eloquent Content';
-	$post->save();
-});
+//Route::get('/basicupdate', function(){
+//	$post = Post::find(2);
+//	$post->title = 'Update Eloquent Title';
+//	$post->content = 'Update Eloquent Content';
+//	$post->save();
+//});
 
-Route::get('/update', function(){
-	Post::where('id', 2)->where('is_admin', 0)->update(['title' => 'NEW PHP TITLE', 'content' => 'I love learning Laravel']);	
-});
+//Route::get('/update', function(){
+//	Post::where('id', 2)->where('is_admin', 0)->update(['title' => 'NEW PHP TITLE', 'content' => 'I love learning Laravel']);	
+//});
 
-Route::get('/delete', function(){
-	$post = Post::find(2);
-	$post->delete();
-});
+//Route::get('/delete', function(){
+//	$post = Post::find(2);
+//	$post->delete();
+//});
 
-Route::get('/delete2', function(){
-	Post::destroy([1,2]);
-});
+//Route::get('/delete2', function(){
+//	Post::destroy([1,2]);
+//});
 
-Route::get('/softdelete', function(){
-	Post::find(4)->delete();
-});
+//Route::get('/softdelete', function(){
+//	Post::find(4)->delete();
+//});
 
-Route::get('/readsoftdelete', function(){
+//Route::get('/readsoftdelete', function(){
 	//$post = Post::find(4);
 	//return $post;
 	
-	$post = Post::onlyTrashed()->get();
-	return $post;
-});
+//	$post = Post::onlyTrashed()->get();
+//	return $post;
+//});
 
-Route::get('/restore', function(){
-	Post::withTrashed()->where('id', 4)->restore();
-});
+//Route::get('/restore', function(){
+//	Post::withTrashed()->where('id', 4)->restore();
+//});
 
-Route::get('/forcedelete', function(){
-	Post::onlyTrashed()->where('is_admin', 0)->forceDelete();
-});
+//Route::get('/forcedelete', function(){
+//	Post::onlyTrashed()->where('is_admin', 0)->forceDelete();
+//});
 
 //One to One Relationships
-Route::get('user/{id}/post', function($id){
-	return User::find($id)->post->title;
-});
+//Route::get('user/{id}/post', function($id){
+//	return User::find($id)->post->title;
+//});
 
 //One to Many Relationship
-Route::get('/posts', function(){
-	$user = User::find(3);
+//Route::get('/posts', function(){
+//	$user = User::find(3);
 	
-	foreach($user->posts as $post){
-		echo $post->title;
-	}
-});
+//	foreach($user->posts as $post){
+//		echo $post->title;
+//	}
+//});
 
 //Many to Many Relationship 
-Route::get('/user/{id}/role', function($id){
-		$user = User::find($id)->roles()->orderBy('id', 'desc')->get();
-		return $user;
+//Route::get('/user/{id}/role', function($id){
+//		$user = User::find($id)->roles()->orderBy('id', 'desc')->get();
+//		return $user;
 		//foreach($user->roles as $role){
 		//	return $role->name;
 		//}
-});
+//});
 
 //Accessing the Intermediate table/pivot
-Route::get('user/pivot', function(){
-	$user = User::find(1);
+//Route::get('user/pivot', function(){
+//	$user = User::find(1);
 	
-	foreach($user->roles as $role){
-			echo $role->pivot->created_at;
-	}
-});
+//	foreach($user->roles as $role){
+//			echo $role->pivot->created_at;
+//	}
+//});
 
 //latihan
-Route::get('user/{id}/pivot', function($id){
-	$user = User::find($id);
+//Route::get('user/{id}/pivot', function($id){
+//	$user = User::find($id);
 	
-	foreach($user->roles as $role){
-			echo $role->pivot->created_at;
-	}
-});
+//	foreach($user->roles as $role){
+//			echo $role->pivot->created_at;
+//	}
+//});
 
 //latihan
-Route::get('/user/{id}/posts', function($id){
-	$user = User::find($id);
+//Route::get('/user/{id}/posts', function($id){
+//	$user = User::find($id);
 	
-	foreach($user->posts as $post){
-		echo $post->title;
-	}
-});
+//	foreach($user->posts as $post){
+//		echo $post->title;
+//	}
+//});
 
-Route::get('/post/{id}/user', function($id){
-	return Post::find($id)->user->name;
-});
+//Route::get('/post/{id}/user', function($id){
+//	return Post::find($id)->user->name;
+//});
 
 //Has MAny through relation
-Route::get('/user/country', function(){
-	$country = Country::find(1);
+//Route::get('/user/country', function(){
+//	$country = Country::find(1);
 	
-	foreach($country->posts as $post){
-			return $post->title;
-	}
-});
+//	foreach($country->posts as $post){
+//			return $post->title;
+//	}
+//});
 
 //Latihan membuat pilihan country dinamis dgn route
-Route::get('/user/country/{id}', function($id){
-	$country = Country::find($id);
+//Route::get('/user/country/{id}', function($id){
+//	$country = Country::find($id);
 	
-	foreach($country->posts as $post){
-			return $post->title;
-	}
-});
+//	foreach($country->posts as $post){
+//			return $post->title;
+//	}
+//});
 
 
 //Polymorphic relations
-Route::get('/post/photos', function(){
-	$post = Post::find(1);
+//Route::get('/post/photos', function(){
+//	$post = Post::find(1);
 	
-	foreach($post->photos as $photo){
-		return $photo->path;
-	}
-});
+//	foreach($post->photos as $photo){
+//		return $photo->path;
+//	}
+//});
 
-Route::get('/user/photos', function(){
-	$user = User::find(1);
+//Route::get('/user/photos', function(){
+//	$user = User::find(1);
 	
-	foreach($user->photos as $photo){
-			return $photo->path;
-	}
-});
+//	foreach($user->photos as $photo){
+//			return $photo->path;
+//	}
+//});
 
 //Latihan modifikasi foto dr user/post tampil sesuai id pada Url
-Route::get('/post/photos/{id}', function($id){
-	$post = Post::find($id);
+//Route::get('/post/photos/{id}', function($id){
+//	$post = Post::find($id);
 	
-	foreach($post->photos as $photo){
-		return $photo->path;
-	}
-});
+//	foreach($post->photos as $photo){
+//		return $photo->path;
+//	}
+//});
 
-Route::get('/user/photos/{id}', function($id){
-	$user = User::find($id);
+//Route::get('/user/photos/{id}', function($id){
+//	$user = User::find($id);
 	
-	foreach($user->photos as $photo){
-			return $photo->path;
-	}
-});
+//	foreach($user->photos as $photo){
+//			return $photo->path;
+//	}
+//});
 
 //Polymorphic Relation the inverse
-Route::get('photo/{id}/post', function($id){
-	$photo = Photo::findOrFail($id);
-	return $photo->imageable;
-});
+//Route::get('photo/{id}/post', function($id){
+//	$photo = Photo::findOrFail($id);
+//	return $photo->imageable;
+//});
 
 //Polymorphic Many to Many
-Route::get('/post/tag', function(){
-	$post = Post::find(1);
-	foreach($post->tags as $tag){
-			echo $tag->name;
-	}
-});
+//Route::get('/post/tag', function(){
+//	$post = Post::find(1);
+//	foreach($post->tags as $tag){
+//			echo $tag->name;
+//	}
+//});
 
 //Latihan Polymorphic Many to Many menjadi dinamis dengan Url
-Route::get('/post/tag/{id}', function($id){
-	$post = Post::find($id);
-	foreach($post->tags as $tag){
-			echo $tag->name;
-	}
-});
+//Route::get('/post/tag/{id}', function($id){
+//	$post = Post::find($id);
+//	foreach($post->tags as $tag){
+//			echo $tag->name;
+//	}
+//});
 
 //Latihan Polymorphic Many to Many menjadi dinamis dengan url(video)
-Route::get('/video/tag/{id}', function($id){
-	$video = Video::find($id);
-	foreach($video->tags as $tag){
-			echo $tag->name;
-	}
-});
+//Route::get('/video/tag/{id}', function($id){
+//	$video = Video::find($id);
+//	foreach($video->tags as $tag){
+//			echo $tag->name;
+//	}
+//});
 
 //Polymorphic Many to Many - retrieving owner
-Route::get('/tag/post', function(){
-	$tag = Tag::find(1);
-	foreach($tag->posts as $post){
-			echo $post->title;
-	}
-});
+//Route::get('/tag/post', function(){
+//	$tag = Tag::find(1);
+//	foreach($tag->posts as $post){
+//			echo $post->title;
+//	}
+//});
 
 
-;
+
+
+Route::resource('/post', 'PostController');
 
 
 /*
