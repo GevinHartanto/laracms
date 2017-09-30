a513dd440f687ed57ac84a51976b02ed4fb70812
GevinHartanto <gevinhartanto@gmail.com>
2017-09-30 10:07:51 +0700

    Modifying Edit & Delete Forms with the Form Package

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -6,7 +6,35 @@
 
 	<h1>Edit Post</h1>
 	
-	<form action="/posts/{{$post->id}}" method="post">
+	<!-- <form action="/posts/{{$post->id}}" method="post">
+		
+		<input type="hidden" name="_method" value="PUT" />
+		
+		<input type="text" name="title" value="{{$post->title}}" />
+		
+		{{csrf_field()}}
+		
+		<input type="submit" name="submit" value="CREATE" />
+	
+	</form -->
+	
+	{!! Form::model($post, ['method'=>'PATCH', 'action'=>['PostController@update', $post->id]]) !!}
+	
+		{!! Form::label('title', 'Title:') !!}
+		{!! Form::text('title', null, ['class'=>'form-control']) !!}
+		{!! Form::submit('Update Post', ['class'=>'btn btn-info']) !!}
+	 
+	 {!! Form::close()!!}
+	
+	{!! Form::open(['method'=>'DELETE', 'action'=>['PostController@destroy', $post->id]]) !!}
+		
+		{!! Form::submit('Delete Post', ['class'=>'btn btn-danger']) !!}
+		
+	{!! Form::close() !!}
+	
+	
+	
+	<!--<form action="/posts/{{$post->id}}" method="post">
 		<input type="hidden" name="_method" value="PUT" />
 		<input type="text" name="title" value="{{$post->title}}" />
 		
@@ -14,13 +42,14 @@
 		
 		<input type="submit" name="submit" value="CREATE" />
 		
-	</form>
+	</form -->
+
 	
-	<form action="/post/{{$post->id}}" method="post">
+	<!--form action="/post/{{$post->id}}" method="post">
 		<input type="hidden" name="_method" value="DELETE" />
 		{{csrf_field()}}
 		<input type="submit" value="DELETE" />
-	</form>
+	</form-->
 	
 @stop
 
