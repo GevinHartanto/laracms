b299f39da32e2b1bf719bf873da5052fa48568a0
GevinHartanto <gevinhartanto@gmail.com>
2017-09-30 10:20:59 +0700

    Advance Validation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreatePostRequest;
 use App\Post;
 use Illuminate\Http\Request;
 
@@ -49,7 +50,7 @@ class PostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreatePostRequest $request)
     {
 		$this->validate($request, [
 			'title' => 'required',
